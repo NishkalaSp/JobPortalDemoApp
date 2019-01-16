880439858a0be286407c424daa9efde7e9ff3d8d
Nishkala Sp <nishkala_p@ndvor.com>
2019-01-16 15:58:06 +0530

    Created DataLayer class library, added DbContext class and enabled migrations for DataLayer.

---
+++ JobPortalDemoApp/DataLayer/JPDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    class JPDbContext : DbContext
+    {
+        public JPDbContext()
+            : base(ConfigurationManager.ConnectionStrings["JPDbConnectionString"].ConnectionString)
+        {
+                
+        }
+    }
+}
