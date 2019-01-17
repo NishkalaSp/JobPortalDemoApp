d2f86532deca3ce937d3d4944177541287cad81c
Nishkala Sp <nishkala_p@ndvor.com>
2019-01-17 18:14:27 +0530

    Added view with tabs to collect job seeker's personal, education and work experience details.

---
+++ JobPortalDemoApp/DataLayer/Entities/ExperienceDetails.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Entities
+{
+    class ExperienceDetails
+    {
+        public int Id { get; set; }
+
+        public User Seeker { get; set; }
+
+        public string CompanyName { get; set; }
+
+        public string JobTitle { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public JobType Type { get; set; }
+
+        public string City { get; set; }
+
+        public string State { get; set; }
+
+        public string Country { get; set; }
+    }
+}
--- JobPortalDemoApp/DataLayer/Entities/JobType.cs
+++ JobPortalDemoApp/DataLayer/Entities/JobType.cs
@@ -10,6 +10,6 @@ namespace DataLayer.Entities
     {
         public int Id { get; set; }
 
-        public string Name { get; set; }
+        public string Type { get; set; }
     }
 }
