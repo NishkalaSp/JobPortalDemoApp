850f0f3b9e9eec3e3636a9a444ed1664c92947e7
Nishkala Sp <nishkala_p@ndvor.com>
2019-01-18 18:57:41 +0530

    Added datatable.

--- JobPortalDemoApp/JobPortalDemoApp/Controllers/JobController.cs
+++ JobPortalDemoApp/JobPortalDemoApp/Controllers/JobController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DataLayer;
+using DataLayer.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,10 +11,36 @@ namespace JobPortalDemoApp.Controllers
     [Authorize]
     public class JobController : Controller
     {
-        // GET: Job
+        private JPDbContext _context;
+
+        public JobController()
+        {
+            _context = new JPDbContext();
+        }
+
         public ActionResult Search()
         {
             return View();
+        }
+
+        public ActionResult GetGridData(string searchString)
+        {
+            var data = GetJobSeekers(searchString).Select(js => new {
+                Id = js.Id,
+                FullName = js.FirstName + " " + js.LastName,
+                Email = js.Email,
+                ContactNumber = js.ContactNumber
+            });
+
+            return Json(new { data = data, recordsTotal = data.Count() } , JsonRequestBehavior.AllowGet);
+        }
+
+        private List<User> GetJobSeekers(string searchString)
+        {
+            var seekerUserType = _context.UserTypes.First(ut => ut.Type.Equals("JobSeeker"));
+            return _context.User.Where(u => u.Type.Id == seekerUserType.Id 
+                                            && (u.FirstName.Contains(searchString)
+                                            || u.LastName.Contains(searchString))).ToList();
         }
     }
 }
